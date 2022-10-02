f3b9f50cdd5f23ecd7c5ed9316e8f5aadb9efd51
Gabriel Terwesten <gabriel@terwesten.net>
2022-10-02 22:46:39 +0200

    feat: support installing `daco` locally (#26)

--- packages/daco/lib/src/command_runner.dart
+++ packages/daco/lib/src/command_runner.dart
@@ -1,6 +1,10 @@
 // ignore_for_file: public_member_api_docs
 
+import 'dart:async';
+import 'dart:io';
+
 import 'package:args/command_runner.dart';
+import 'package:cli_launcher/cli_launcher.dart';
 
 import 'commands/analyze.dart';
 import 'commands/format.dart';
@@ -25,4 +29,27 @@ class DacoCommandRunner extends CommandRunner<void> {
   ///
   /// If not specified, a logger will be created based on the verbose flag.
   final DacoLogger? logger;
+}
+
+class DacoLauncher extends Launcher {
+  DacoLauncher() : super(PackageExecutable('daco', 'daco'));
+
+  @override
+  Future<void> run(
+    List<String> arguments,
+    InstallationLocation location,
+  ) async {
+    try {
+      await DacoCommandRunner().run(arguments);
+    } on UsageException catch (error) {
+      exitCode = 1;
+      stderr.write(error);
+      // ignore: avoid_catches_without_on_clauses
+    } catch (error, stackTrace) {
+      exitCode = 1;
+      stderr
+        ..writeln('Unexpected error: $error')
+        ..writeln(stackTrace);
+    }
+  }
 }
